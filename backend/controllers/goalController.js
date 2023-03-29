5631bb8974bd89220aa31e93a3d46d08b3e8d364
unknown <helmetnipapa@gmail.com>
2023-03-29 20:01:47 +0800

    added mongo database code

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -2,34 +2,36 @@
 /*         Purpose of a controller: to handle the logic of the routes         */
 /* -------------------------------------------------------------------------- */
 
+import asyncHandler from "express-async-handler";
+
 // @desc Get goals
 // @route GET /api/goals
 // @access Private
-export const getGoals = (req, res) => {
+export const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get goals" });
-};
+});
 
 // @desc Set goal
 // @route POST /api/goals
 // @access Private
-export const setGoal = (req, res) => {
+export const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please add a text field");
   }
   res.status(200).json({ message: "Set goals" });
-};
+});
 
 // @desc Create goal
 // @route PUT /api/goals/:id
 // @access Private
-export const createGoal = (req, res) => {
+export const createGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update goal ${req.params.id}` });
-};
+});
 
 // @desc Delete a goal
 // @route DELETE /api/goals/:id
 // @access Private
-export const deleteGoal = (req, res) => {
+export const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete goal ${req.params.id}` });
-};
+});
--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,14 @@ import express from "express";
 import dotenv from "dotenv";
 import goalRoutes from "./routes/goalRoutes.js";
 import { errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+import colors from "colors";
 
 const env = dotenv.config();
 const port = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
 
 // Initialize express
 const app = express();
